26214c0c7064c78282de7985e160413a5ef31aaa
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-07-17 12:55:23 +0600

    eachProject done

--- src/components/home/EachProject.js
+++ src/components/home/EachProject.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { TbLivePhoto } from 'react-icons/tb';
 import { AiOutlineGithub } from 'react-icons/ai';
+import './EachProject.css';
 
 const EachProject = () => {
     const [eachProject, setEachProject] = useState({});
@@ -15,18 +16,39 @@ const EachProject = () => {
             image: "https://i.ibb.co/34NjL41/carFront.png",
             stack: "Full-Stack",
             description: "This is a full-stack responsive react project where dealers can buy tools in bulk. Users can go to the dashboard and pay for or cancel their orders. Only admins see all orders and their payment status in the dashboard. They can also ship paid orders. Admins can make normal users admin and add or delete products. Data stored in mongodb database. Used jwt Token for more secure.",
-            links: [
-                {
-                    id: 32,
-                    title: "Client Code",
-                    url: "https://github.com/mohiuddin2721/bd_car_parts_jm_12"
-                },
-                {
-                    id: 33,
-                    title: "Server Code",
-                    url: "https://github.com/mohiuddin2721/bd-car-parts-server-jm-12"
-                }
+            technology: [
+                "Html",
+                "Css",
+                "JavaScript",
+                "React",
+                "Tailwind",
+                "DaisyU",
+                "ExpresJs",
+                "MongoDB",
+                "Stripe",
             ],
+            screenshots: [
+                "https://i.ibb.co/DW06KGd/car1.png",
+                "https://i.ibb.co/XWnS4MC/car2.png",
+                "https://i.ibb.co/gTqNvC8/car3.png",
+                "https://i.ibb.co/rKh3n8r/car4.png",
+                "https://i.ibb.co/TqN4crL/car5.png",
+                "https://i.ibb.co/SK4cNb7/car6.png",
+                "https://i.ibb.co/2PSzy61/car7.png",
+                "https://i.ibb.co/d4S575w/car-8.png",
+            ],
+            clientLinks:
+            {
+                id: 32,
+                title: "Client Code",
+                url: "https://github.com/mohiuddin2721/bd_car_parts_jm_12"
+            },
+            serverLinks:
+            {
+                id: 33,
+                title: "Server Code",
+                url: "https://github.com/mohiuddin2721/bd-car-parts-server-jm-12"
+            },
             liveLinks:
             {
                 id: 61,
@@ -54,6 +76,34 @@ const EachProject = () => {
                     url: "https://github.com/mohiuddin2721/ac-stock-house-server-jm-11"
                 }
             ],
+            technology: [
+                "Html",
+                "Css",
+                "JavaScript",
+                "React",
+                "Tailwind",
+                "DaisyU",
+                "ExpresJs",
+                "MongoDB",
+            ],
+            screenshots: [
+                "https://i.ibb.co/DgmTxjJ/ac1.png",
+                "https://i.ibb.co/zFdtgH1/ac2.png",
+                "https://i.ibb.co/n6PVm2s/ac3.png",
+                "https://i.ibb.co/gFTdj52/ac4.png",
+            ],
+            clientLinks:
+            {
+                id: 42,
+                title: "Client Code",
+                url: "https://github.com/mohiuddin2721/ac-stock-house-jm-11"
+            },
+            serverLinks:
+            {
+                id: 43,
+                title: "Server Code",
+                url: "https://github.com/mohiuddin2721/ac-stock-house-server-jm-11"
+            },
             liveLinks:
             {
                 id: 62,
@@ -76,6 +126,26 @@ const EachProject = () => {
                     url: "https://github.com/mohiuddin2721/shawon-legal-consultant-jm-10"
                 }
             ],
+            technology: [
+                "Html",
+                "Css",
+                "JavaScript",
+                "React",
+                "Tailwind",
+                "DaisyU",
+            ],
+            screenshots: [
+                "https://i.ibb.co/NNsY7Nz/legal1.png",
+                "https://i.ibb.co/rmTgP14/legal2.png",
+                "https://i.ibb.co/4dybnbF/legal3.png",
+                "https://i.ibb.co/NmJ20c3/legal4.png",
+            ],
+            clientLinks:
+            {
+                id: 52,
+                title: "Client Code",
+                url: "https://github.com/mohiuddin2721/shawon-legal-consultant-jm-10"
+            },
             liveLinks:
             {
                 id: 63,
@@ -91,27 +161,78 @@ const EachProject = () => {
         setEachProject(project)
     }, [id]);
 
-    // console.log(eachProject);
-
-
     return (
-        <div>
-            <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col md:flex-row lg:flex-row">
-                    <img src={eachProject.image} className="max-w-sm rounded-lg shadow-2xl" />
+        <div className="detail-card flex flex-col lg:min-h-screen p-5 lg:p-40 hover:text-white">
+            <div className="inside-detail flex flex-col md:flex-row lg:flex-row justify-center items-start gap-8 lg:h-[60vh] text-left mb-32">
+                <div className="image-detail w-full h-80 lg:w-1/2 lg:h-full rounded-xl overflow-hidden">
+                    <img src={eachProject?.image} alt="" />
+                </div>
+                <div className="w-full lg:w-1/2 flex flex-col h-full justify-center">
                     <div>
-                        <h1 className="text-sm text-green-500 mb-5">{eachProject.name} <span className='text-sm text-green-500 badge h-auto'>| {eachProject.stack} |</span></h1>
-                        <h1 className="text-xl font-bold">{eachProject.type}</h1>
-                        <p className="py-6">{eachProject.description}</p>
-                        <div className='space-x-48'>
-                            {
-                                eachProject?.links?.map(each =>
-                                    <a key={each.id} href={each?.url} target='_blank' className='text-sm btn btn-active hover:border-green-500 btn-xs text-end cursor-pointer'>
-                                        <AiOutlineGithub className='text-blue-500 mr-1'></AiOutlineGithub>{each?.title}</a>)
-                            }
-                            <a href={eachProject?.liveLinks?.url} target='_blank' className='text-sm btn btn-active hover:border-green-500 btn-xs text-end cursor-pointer'><TbLivePhoto className='text-blue-500 mr-1'></TbLivePhoto>Live</a>
+                        <h2 className="font-semibold text-sm my-4 text-secondary">
+                            {eachProject?.name}
+                        </h2>
+                        <h2 className="project-title text-accent font-semibold my-4">
+                            {eachProject?.type}
+                        </h2>
+                        <div className="my-3">
+                            <h3 className=" text-secondary">Project Overview</h3>
+                            <p className='text-sm'>{eachProject?.description}</p>
+                        </div>
+                        <div className="my-2">
+                            <h3 className="text-secondary">Technology used</h3>
+                            <div className="flex flex-wrap mr-2">
+                                {eachProject?.technology?.map((point, index) => (
+                                    <span key={index} className="text-white hover:text-blue-400 btn-xs">
+                                        {point}
+                                    </span>
+                                ))}
+                            </div>
                         </div>
                     </div>
+                    <div className="button-content flex justify-between mt-1">
+                        <a
+                            href={eachProject?.liveLinks?.url}
+                            target="_blank"
+                            rel="noreferrer"
+                            className="project-button text-[10px] lg:text-[12px]"
+                        >
+                            <i className="text-sm text-secondary mr-2"><TbLivePhoto className='text-blue-500'></TbLivePhoto></i>
+                            View Live
+                        </a>
+                        <a
+                            href={eachProject?.clientLinks?.url}
+                            target="_blank"
+                            rel="noreferrer"
+                            className="project-button text-[10px] lg:text-[12px]"
+                        >
+                            <i className="text-sm text-secondary mr-2"><AiOutlineGithub className='text-blue-500'></AiOutlineGithub></i>
+                            Client Code
+                        </a>
+                        <a
+                            href={eachProject?.serverLinks?.url}
+                            target="_blank"
+                            rel="noreferrer"
+                            className="project-button text-[10px] lg:text-[12px]"
+                        >
+                            {" "}
+                            <i className="text-sm text-secondary mr-2"><AiOutlineGithub className='text-blue-500'></AiOutlineGithub></i>
+                            Server Code
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <p className="text-white text-center font-bold text-xl lg:text-5xl mb-10">
+                    Project Screenshots
+                </p>
+                <div className="screenshot w-full grid grid-cols-1 lg:grid-cols-2 gap-10">
+                    {eachProject?.screenshots?.map((screenshot) => (
+                        <div>
+                            <img className="w-full rounded-3xl" src={screenshot} alt="" />
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
